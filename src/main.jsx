46dd02a9809a2308c799edaed15fa00cec49a5bc
fvieira <61894651+Fvieira64@users.noreply.github.com>
2026-06-01 19:48:36 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../brewery-tracker.jsx'
 
-ReactDOM.creatRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
